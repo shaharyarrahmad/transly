2e5fcb29f65bd81b2d9826cfbeaab9ac510c8c14
Shaharyar Ahmad <53024652+shaharyarrahmad@users.noreply.github.com>
2023-04-26 16:05:37 +0500

    changes

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -4,26 +4,26 @@ const API_BASE_URL = 'https://api.assemblyai.com/v2';
 const API_KEY = process.env.ASSEMBLYAI_API_KEY;
 
 export default defineEventHandler(async (event) => {
-  const AudioBucket: KVNamespace = event.context.cloudflare.env.AudioBucket;
-    // upload event file to cloudflare
-    const audioFile = await readBody(event);
-    // const audioFileName = audioFile.name;
-    const audioFileName = `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
-    const audioFileBuffer = await audioFile.arrayBuffer();
-    await AudioBucket.put(audioFileName, audioFile);
-    return { data: audioFileName };
-    // const file = await readBody(event);
-    // try {
-    //   const response = await axios.post(`${API_BASE_URL}/upload`, file, {
-    //     headers: {
-    //       'authorization': API_KEY,
-    //       'Transfer-Encoding': 'chunked',
-    //     },
-    //   });
-    //   const responseData = await response.data;
-    //   console.log('responseData', responseData.upload_url);
-      // return  {data: responseData.upload_url};
-    // } catch (error) {
-    //   return { status: 500, body: { message: 'Error uploading file' } };
-    // }
+  // const AudioBucket: KVNamespace = event.context.cloudflare.env.AudioBucket;
+  //   // upload event file to cloudflare
+    // const audioFile = await readBody(event);
+  //   // const audioFileName = audioFile.name;
+  //   const audioFileName = `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
+  //   const audioFileBuffer = await audioFile.arrayBuffer();
+  //   await AudioBucket.put(audioFileName, audioFile);
+  //   return { data: audioFileName };
+    const file = await readBody(event);
+    try {
+      const response = await axios.post(`${API_BASE_URL}/upload`, file, {
+        headers: {
+          'authorization': API_KEY,
+          'Transfer-Encoding': 'chunked',
+        },
+      });
+      const responseData = await response.data;
+      console.log('responseData', responseData.upload_url);
+      return  {data: responseData.upload_url};
+    } catch (error) {
+      return { status: 500, body: { message: 'Error uploading file' } };
+    }
   });
