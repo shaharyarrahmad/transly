1a6d902c58fd86a27cf88f7466a628706d7fc167
Shaharyar Ahmad <53024652+shaharyarrahmad@users.noreply.github.com>
2023-04-28 13:57:03 +0500

    refactor nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   generate: {
-    routes: [
-      '/'
-    ]
+    routes: ["/"],
   },
-  css: ['@/assets/styles/main.scss'],
+  css: ["@/assets/styles/main.scss"],
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      title: 'Transly',
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Transly",
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'Instant, Free and Accurate Transcription' },
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Instant, Free and Accurate Transcription",
+        },
 
-        { hid: 'og:image', property: 'og:image', content: '/icon.png' },
+        { hid: "og:image", property: "og:image", content: "/icon.png" },
       ],
 
-
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      // { rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' },
-      // {rel: 'apple-touch-icon' , href: '/icon.png'},
-    ],
-    }
-
-  }
-
-})
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+});
